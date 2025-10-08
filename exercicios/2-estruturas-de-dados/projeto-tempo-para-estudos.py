30d844a8d3ab1d9770c289777672adf54b6bab66
andreialatorre-commits <andreia.latorre@gmail.com>
2025-10-08 17:17:23 +0000

    feat: enhance study hours script with user input for name, study days, hours, and course

--- exercicios/2-estruturas-de-dados/projeto-tempo-para-estudos.py
+++ exercicios/2-estruturas-de-dados/projeto-tempo-para-estudos.py
@@ -1,6 +1,12 @@
 # Criaremos um script que imprimirá na tela o total de horas que uma pessoa estudou durante um determinado período:
+
 # 1. Crie uma variável chamada "nome" e, usando o método input(), atribua a ela um nome;
+nome = input('Qual seu nome? ')
 # 2. Crie uma variável chamada "total_dias" e, usando o método input(), solicite o total de dias dedicados ao estudo por semana;
+total_dias = int(input('Quantos dias por semana você estuda? '))
 # 3. Crie uma variável chamada "total_horas" e, usanod o método input(), solicite a média de horas estudada por dia;
+total_horas = int(input('Quantas horas por dia você estuda? '))
 # 4. Crie uma variável chamada "curso" e, usando o método input(), solicite o título do curso desejado;
+curso = input('Qual o nome do curso que você está estudando? ')
 # 5. Imprima na tela uma frase informando o nome da estudante, o total_dias dedicados aos estudos, o total horas semanais e o curso.
+print(f'Olá {nome}, você estuda {total_dias} dias por semana, totalizando {total_dias * total_horas} horas semanais de estudo no curso {curso}.')
