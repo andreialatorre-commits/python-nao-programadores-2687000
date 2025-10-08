036082c080579e0760c6ff9c6a12f4ca45f0e531
andreialatorre-commits <andreia.latorre@gmail.com>
2025-10-08 18:10:27 +0000

    refactor: clean up conditional structure and improve print statements

--- exercicios/3-estruturas-de-controle/1-estrutura-condicional.py
+++ exercicios/3-estruturas-de-controle/1-estrutura-condicional.py
@@ -1,18 +1,20 @@
 # Declare 4 variáveis do tipo numérica
-
+x = 10
+y = 20
+a = 5
+b = 15
 
 # Crie uma estrutura condicional para comparar dois números
-
+if (x > y) and (x > a):
+    print(f'A condição foi cumprida. O número {x} é menor que o número {y} e é maior que o número {a} ')
+elif y > x:
+    print(f'Verificamos que o número {y} é maior que o número {x}')
+else:
+    print(f'A condição não foi cumprida, pois o número {y} é maior que o número {x}')    
 
 # Se a condição for verdadeira, imprima na tela uma mensagem informando que a condição foi cumprida e informando o número de maior valor
-
-
 # Se a condição não for cumprida, imprima na tela uma mensagem informando que a condição é negativa e informe o número de maior valor
-
-
 # Insira outras condições na estrutura condicional usando o elif
-
-
 # Incremente a estrutura condicional já existente com expressões lógicas utilizando "and" ou "or"
 
 
