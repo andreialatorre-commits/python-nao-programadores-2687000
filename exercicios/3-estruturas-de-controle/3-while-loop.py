16ee9bf63fc281bd76346cda085452f78d9f6d2e
andreialatorre-commits <andreia.latorre@gmail.com>
2025-10-17 17:32:44 +0000

    feat: add else clause to while loop for completion message

--- exercicios/3-estruturas-de-controle/3-while-loop.py
+++ exercicios/3-estruturas-de-controle/3-while-loop.py
@@ -1,7 +1,11 @@
 # Crie duas variáveis do tipo numérica, uma sinalizando a fase atual do curso e outra o nível final
-
+nivel_atual = 1
+nivel_final = 5
 
 # Crie um while loop que imprima na tela o nível atual
-
-
+while nivel_atual <= nivel_final:
+  print(f'Você está no nível {nivel_atual} do curso.')
+  nivel_atual += 1
+else:
+  print('Parabéns! Você completou todos os níveis do curso.')
 # Insira "else" no while loop anterior.
