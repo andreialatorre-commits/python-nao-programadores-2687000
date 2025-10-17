9837a790c8055738d44ad4c70357606971c4c5e9
andreialatorre-commits <andreia.latorre@gmail.com>
2025-10-17 17:21:14 +0000

    feat: implement FizzBuzz program to replace numbers with words in a list

--- exercicios/3-estruturas-de-controle/projeto-fizz-buzz.py
+++ exercicios/3-estruturas-de-controle/projeto-fizz-buzz.py
@@ -6,3 +6,20 @@
 # 3.2 Caso o número seja divisível por 5, substitua-o por "Buzz"
 # 3.3 Caso o número seja divisível por 3 e 5, substitua-o por "FizzBuzz"
 
+lista_numeros = list(range(15,31))
+indice = 0
+print(lista_numeros)
+
+for numero in lista_numeros:
+    if numero % 3 == 0 and numero % 5 == 0:
+        lista_numeros[indice] = "FizzBuzz"
+    elif numero % 3 == 0:
+        lista_numeros[indice] = "Fizz"
+    elif numero % 5 == 0:
+        lista_numeros[indice] = "Buzz"
+    else:
+        lista_numeros[indice] = numero
+
+    indice += 1 
+print(lista_numeros) 
+    
